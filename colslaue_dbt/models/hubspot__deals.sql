263ef3c1b4bc6c6e96a4e2dd7998136193aa65ba
colslaue <colin.slauenwhite@dashhudson.com>
2024-04-10 21:07:59 -0300

    Add support for `hubspot.company` and `hubspot.deal_company`

--- colslaue_dbt/models/hubspot__deals.sql
+++ colslaue_dbt/models/hubspot__deals.sql
@@ -1,11 +1,16 @@
 {{ config(schema='staging') }}
 
 SELECT
-    id AS deal_id
+    deal.id AS deal_id
+    ,company.id AS company_id
     ,dealname AS deal_name
-    ,DATE(createdate) AS create_date
-    ,DATE(closedate) AS close_date
+    ,DATE(deal.createdate) AS create_date
+    ,DATE(deal.closedate) AS close_date
     ,pipeline AS pipeline
     ,dealstage AS deal_stage
     ,amount AS amount
-FROM {{ source('hubspot', 'deal') }}
+FROM {{ source('hubspot', 'deal') }} AS deal
+LEFT JOIN {{ source('hubspot', 'deal_company') }} AS deal_company
+ON deal.id = deal_company.deal_id
+LEFT JOIN {{ source('hubspot', 'company') }} AS company
+ON company.id = deal_company.companyid
