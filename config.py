9191dca4e58d7d352f8a9781f2d200297b451f01
colslaue <colin.slauenwhite@dashhudson.com>
2025-08-29 20:17:12 -0300

    Add env variables to `config.py`

--- config.py
+++ config.py
@@ -6,5 +6,7 @@ load_dotenv()
 class Config:
     GOOGLE_APPLICATION_CREDENTIALS=os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
     HUBSPOT_API_KEY=os.environ.get("HUBSPOT_API_KEY")
+    BIGQUERY_PROJECT=os.environ.get("BIGQUERY_PROJECT")
+    BIGQUERY_HUBSPOT_DATASET=os.environ.get("BIGQUERY_HUBSPOT_DATASET")
 
 CONFIG = Config()
