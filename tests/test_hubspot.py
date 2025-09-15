e930e29f6d264e16213399676c598bf6bf996885
colslaue <colin.slauenwhite@dashhudson.com>
2025-09-14 22:12:00 -0300

    fix `test_hubspot.py`

--- tests/test_hubspot.py
+++ tests/test_hubspot.py
@@ -1,30 +1,30 @@
 from workers.tasks import (
-    CONFIG,
     hubspot_deals_to_bigquery,
 )
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, ANY
 
 
 class TestHubspotDealsToBigQuery(unittest.TestCase):
     @patch('workers.tasks.HubspotConn')
     @patch('workers.tasks.BigQueryClient')
+    @patch('workers.tasks.CONFIG')
     def test_export_logic_with_mocks(
-        self, mock_BigQueryClient, mock_HubspotConn
+        self, mock_CONFIG, mock_BigQueryClient, mock_HubspotConn
     ):
 
-        mock_row = Mock()
-        mock_row.id = 12345
-        mock_row.amount = 123.45
-        mock_row.deal_name = "deal name"
-        mock_row.deal_stage = "deal stage"
-        mock_row.pipeline = "pipeline"
+        mock_CONFIG.BIGQUERY_PROJECT = "bigquery_project"
+        mock_CONFIG.BIGQUERY_HUBSPOT_DATASET = "bigquery_hubspot_dataset"
+        mock_CONFIG.HUBSPOT_API_KEY = "hubspot_api_key"
+        mock_CONFIG.GOOGLE_APPLICATION_CREDENTIALS = "google_application_credentials"
+        mock_session = mock_HubspotConn.return_value.get_instance.return_value
+        mock_session.crm.deals.basic_api.get_page.return_value = Mock(
+            to_dict=lambda: {'results': [
+                {'id': 12345, 'properties': {'amount': 123.45, 'dealname': 'deal name', 'dealstage': 'deal stage', 'pipeline': 'pipeline'}}
+            ]}
+        )
+        mock_bq_client = mock_BigQueryClient.return_value.get_instance.return_value
 
-        mock_results = [mock_row]
-
-        mock_session = mock_HubspotConn.get_instance.return_value.__enter__.return_value
-        mock_session.crm.deals.basic_api.get_page.return_value = mock_results
-        mock_bq_client = mock_BigQueryClient.get_instance.return_value.__enter__.return_value
 
         hubspot_deals_to_bigquery()
 
@@ -32,7 +32,7 @@ class TestHubspotDealsToBigQuery(unittest.TestCase):
 
         expected_output = [
             {
-                "id": 12345,
+                "deal_id": 12345,
                 "amount": 123.45,
                 "deal_name": "deal name",
                 "deal_stage": "deal stage",
@@ -41,6 +41,10 @@ class TestHubspotDealsToBigQuery(unittest.TestCase):
         ]
 
         expected_table_id = (
-            f"{CONFIG.BIGQUERY_PROJECT}.{CONFIG.BIGQUERY_HUBSPOT_DATASET}.deal"
+            f"{mock_CONFIG.BIGQUERY_PROJECT}.{mock_CONFIG.BIGQUERY_HUBSPOT_DATASET}.deal"
         )
-        mock_bq_client.load_table_from_json.assert_called_once_with(expected_output, expected_table_id)
+        mock_bq_client.load_table_from_json.assert_called_once_with(
+            json_rows=expected_output,
+            destination=expected_table_id,
+            job_config=ANY,
+        )
