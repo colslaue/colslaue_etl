c577a88cbe5436bdbd068833545d2757aeebe938
colslaue <colin.slauenwhite@dashhudson.com>
2025-09-15 10:44:35 -0300

    test change to diff coverage

--- workers/tasks.py
+++ workers/tasks.py
@@ -36,3 +36,8 @@ def hubspot_deals_to_bigquery():
         destination=f"{CONFIG.BIGQUERY_PROJECT}.{CONFIG.BIGQUERY_HUBSPOT_DATASET}.deal",
         job_config=job_config
     )
+
+def test_diff():
+    print("Hello, World")
+    test = "Hello"
+    test2 = "World"
