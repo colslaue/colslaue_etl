07311e6fee2b23758efb9f9da2e95773a3edd9ad
colslaue <colin.slauenwhite@dashhudson.com>
2025-09-16 10:40:53 -0300

    Test `test.yml`

--- workers/tasks.py
+++ workers/tasks.py
@@ -36,3 +36,6 @@ def hubspot_deals_to_bigquery():
         destination=f"{CONFIG.BIGQUERY_PROJECT}.{CONFIG.BIGQUERY_HUBSPOT_DATASET}.deal",
         job_config=job_config
     )
+
+def test_func():
+    print('test')
